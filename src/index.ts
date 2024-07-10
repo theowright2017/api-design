0438638995c27b17f61cbd4ae36fa807f7d46303
Theo Wright <theowright2015@gmail.com>
2024-07-10 13:36:13 +0100

    2/ - added routes and middleware - middleware configs api prior to hitting endpoint - routes are defined manually

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-const app = require('./server')
+import app from './server'
 
 app.listen(3005, () => {
     console.log(' hello from 3005')
---
+++ src/server.ts
@@ -0,0 +1,56 @@
+import express from 'express'
+import router from './router'
+import morgan from 'morgan'
+import cors from 'cors'
+
+ const app = express()
+
+/* ---  extract to Middleware file? --- */
+
+app.use(cors())
+
+/**
+ * global mount for all calls
+ * (morgan logs all calls)
+ */
+app.use(morgan('dev'))
+
+/**
+ *  middleware
+ *      - allows client to send us json
+ */
+app.use(express.json())
+
+/**
+ *  allows url decoding
+ */
+app.use(express.urlencoded({extended: true}))
+
+/**
+ * More middleware
+ */
+app.use((req, res, next) => {
+    // res.status(400)
+    // res.send('Nope')
+    
+    next()
+})
+
+/* ---  end of Middleware --- */
+
+
+app.get('/', (req, res) => {
+    console.log('hello express')
+    res.status(200)
+    res.json({message: 'hello from inner get /'})
+})
+
+/**
+ * mount to methods in ./router
+ *  /api becomes prefix for all router methods
+ */
+app.use('/api',  router)
+
+
+
+export default app
